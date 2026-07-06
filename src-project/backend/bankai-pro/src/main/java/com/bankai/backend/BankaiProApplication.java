e0cee48ca4c8fa3fa0b0ab3624721020c6799585
Nikhil Khandare <nikhil.khandare.cmfeb26@gmail.com>
2026-07-06 21:46:58 +0530

    Add initial business rules documentation

---
+++ src-project/backend/bankai-pro/src/main/java/com/bankai/backend/BankaiProApplication.java
@@ -0,0 +1,14 @@
+package com.bankai.backend;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankaiProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankaiProApplication.class, args);
+	}
+
+}
---
+++ src-project/backend/bankai-pro/src/test/java/com/bankai/backend/BankaiProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankaiProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
