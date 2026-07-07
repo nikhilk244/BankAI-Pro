c0ba3672492f96fbae94db4ab20524f7de234d50
Nikhil Khandare <nikhil.khandare.cmfeb26@gmail.com>
2026-07-07 18:13:30 +0530

    Add BaseEntity for the common entity fields

--- src-project/backend/bankai-pro/src/main/java/com/bankai/backend/entity/Customer.java
+++ src-project/backend/bankai-pro/src/main/java/com/bankai/backend/entity/Customer.java
@@ -1,5 +1,18 @@
 package com.bankai.backend.entity;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Builder
+@Entity
+@Table(name ="customers")
 public class Customer {
 
+	
 }
